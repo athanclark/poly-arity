142237d67b1b1ff3e08b4b0ed81da9e8b8ec222c
Athan Clark <athan.clark@gmail.com>
2015-04-25 11:53:28 -0600

    expectation and construction

--- src/Data/Function/Poly.hs
+++ src/Data/Function/Poly.hs
@@ -1,3 +1,22 @@
+{-# LANGUAGE
+    TypeFamilies
+  , KindSignatures
+  , DataKinds
+  , ConstraintKinds
+  , TypeOperators
+  #-}
+
 module Data.Function.Poly where
 
-import Data.Function.Poly.Internal
+import Data.Constraint
+
+
+type family TypeListToArity (xs :: [*]) (r :: *) :: * where
+  TypeListToArity '[] r = r
+  TypeListToArity (x ': xs) r = x -> TypeListToArity xs r
+
+type family ExpectArity (xs :: [*]) (f :: *) :: Constraint where
+  ExpectArity '[] f = () -- basis
+  ExpectArity (x ': xs) (x -> remainder) = ExpectArity xs remainder
+
+-- class ConsumeArity
